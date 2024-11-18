95917730ede6ad09a42016ed9aa21490105f9208
SisypheanOdyssey <184602237+SisypheanOdyssey@users.noreply.github.com>
2024-11-18 11:30:36 -0800

    Create first working version of this RPS game
    The attached .js file contains the first working version of the Rock,
    Paper, Scissors game.
    
    It is able to:
        1) Randomly generate integer 1, 2, or 3; range not hard-coded
        2) Assign each randomized output to Rock, Paper, or Scissor
        3) Define and return the Computer's choice based on above
        4) Prompt the Human player for input + store as variable
        5) Filter out any inputs that are not Rock, Paper or Scissor
        6) Update the player input variable to be that input
        7) Create a function to decide which player wins
        8) Log message announcing winner or tie if same hand
        9) Increment the score of either player by 1 if win

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
-console.log("hello world");
+console.log("Shall we play some Rock, Paper, Scissors?");
+
+// Step 1
+const choiceOne = "Rock";
+const choiceTwo = "Paper";
+const choiceThree = "Scissor";
+
+function getComputerChoice(flex) {
+    return Math.ceil(Math.random() * flex);
+}
+
+const choice = getComputerChoice(3);
+console.log(choice);
+
+// Step 2
+let npc = 
+    (choice === 1) ?
+    choiceOne :
+    (choice === 2) ?
+    choiceTwo :
+    choiceThree;
+
+console.log(npc);
+
+// Step 3
+const input = prompt("Please enter Rock, Paper, or Scissor.", "Rock");
+let player;
+
+switch (input) {
+    case "Rock":
+        alert(input);
+        player = input;
+        break;
+    case "Paper":
+        alert(input);
+        player = input;
+        break;
+    case "Scissor":
+        alert(input);
+        player = input;
+        break;
+    default:
+        alert ("Please enter a valid choice. Capitalization matters.");
+}
+
+console.log(player);
+
+// Step 4
+let humanScore = 0;
+let computerScore = 0;
+
+// Step 5
+function singleRound(npc, player) {
+
+if (
+    (npc === "Rock" && player === "Scissor")
+    || (npc === "Paper" && player === "Rock")
+    || (npc === "Scissor" && player === "Paper")
+    ) {
+        console.log("The Computer Wins!");
+        computerScore++;
+    } else if (
+        (player === "Rock" && npc === "Scissor")
+        || (player === "Paper" && npc === "Rock")
+        || (player === "Scissor" && npc === "Paper")
+        ) {
+            console.log("The Human Wins!");
+            humanScore++;    
+        } else {
+            console.log("It's a Tie!");
+         }
+}  
+
+singleRound(npc, player);
+    
+console.log(computerScore);
+console.log(humanScore); 
