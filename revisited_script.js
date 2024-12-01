e1670a2d7880acbd3ab8fe7ea665520f0df4c9a8
SisypheanOdyssey <184602237+SisypheanOdyssey@users.noreply.github.com>
2024-12-01 13:38:19 -0800

    Pushing unmerged commits to GitHub for system migration due to buggy VM
    I just realized that using Windows Subsystem for Linux is much faster
    than running it on VirtualBox, and I encounter significantly less
    technical issues and unstable operations. I have set up a dev
    environment on the WSL Ubuntu that closely mirrors the tech stack of
    this operating system.
    
    Live long and prosper.

--- revisited_script.js
+++ revisited_script.js
@@ -68,9 +68,6 @@ score.setAttribute("style", "border: 4px solid gold; border-radius: 10px; paddin
 score.textContent = `The Score is: \n Stupid Human ${humanScore} || Evil Bot 2000 ${botScore} \n ${message}`
 body.appendChild(score)
 
-
-// event listener
-// let stupidHuman
 /*
 buttonRock.addEventListener("click", () => {
   stupidHuman = buttonRock.textContent.toLowerCase()
@@ -102,125 +99,10 @@ function theWinnerIs(evilBot2000, stupidHuman) {
 
 } 
 
-//
-
 let stupidHuman
 
 buttonRock.addEventListener("click", theWinnerIs)
 console.log(stupidHuman)
-
-/*
-console.log(theWinnerIs(evilBot2000, stupidHuman))
-
-
-
-buttonRock.addEventListener("click", theWinnerIs)
-
-
-buttonRock.addEventListener("click", () => {
-    stupidHuman = buttonRock.textContent.toLowerCase()
-    console.log(stupidHuman)
-    console.log(evilBot2000)
-    console.log(botScore)
-    console.log(humanScore)
-
-})
-
-buttonPaper.addEventListener("click", () => {
-  stupidHuman = buttonPaper.textContent.toLowerCase()
-  theWinnerIs(evilBot2000, stupidHuman)
-  console.log(botScore);
-  console.log(humanScore);
-})
-
-buttonScissor.addEventListener("click", () => {
-  stupidHuman = buttonScissor.textContent.toLowerCase()
-  theWinnerIs(evilBot2000, stupidHuman)
-  console.log(botScore);
-  console.log(humanScore);
-})
-/*
-//=====
-
-
-
-function playRound () {  // function declaration
-  
-  randomNumber();
-  const num = randomNumber();
-  
-  const evilBot2000 = getComputerChoice(num);
-  console.log(evilBot2000);
-  
-  i += 1;
-  let input = prompt(`Round ${i}. Please enter rock, paper, or scissor.`, "rock, paper, scissor").toLowerCase();
-  const stupidHuman = getHumanChoice(input);
-  console.log(stupidHuman);
-  
-  theWinnerIs(evilBot2000, stupidHuman);
-  
-  console.log(botScore);
-  console.log(humanScore);
-  
-  console.log(`==========END OF ROUND ${i}===========`);
-
-};
-
-
-
-/*
-
-
-for ( let j = 0; j < 4; j++ ) {  // 'i < 4' b/c first round played before loop (step 1)
-  playRound();  
-};
-
-// === End of Looping Function ===
-
-
-console.log(`The Final Score is [Human: ${humanScore}] [EvilBot2000: ${botScore}]`);
-
-let winner;
-if (botScore > humanScore) winner = "EvilBot2000";
-    else if (humanScore > botScore) winner = "Human";
-        else winner = "Everyone and No One all at Once!";
-
-console.log(`And the winner of this exciting 5-round matchup is: ${winner}`);
-
-console.log("==========END OF MATCH===========");
-*/
-
-
-//===== OLD =======
-/*
-let i = 1;
-let input = prompt(`Round ${i}. Please enter rock, paper, or scissor.`, "rock, paper, scissor").toLowerCase();
-
-if (input === "rock" || input === "paper" || input === "scissor") { 
-  // added to account for any other value entered
-  
-  function getHumanChoice(input) {
-    
-    if (input === "rock") return "rock"; // removed bracket b/c I learned I could
-    
-      else if (input === "paper") return "paper";
-    
-        else return "scissor";
- 
-  };
-
-} else alert("Invalid Entry. Please try again.");
-
-const stupidHuman = getHumanChoice(input);
-// console.log(stupidHuman);
-
-// === End of Human Output === Step Validated
-
-
-
-  
-
-theWinnerIs(evilBot2000, stupidHuman);
 
 console.log(botScore);
 console.log(humanScore);
